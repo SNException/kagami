ecf36b9c1e52656897e104cdc3cd956f55af748a
Niklas Schultz <thexsalkin@gmail.com>
2021-09-14 17:46:54 +0200

    forget to update build.bat to OpenJDK 17

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@
 set SRC_DIR=src
 set OUT_DIR=bin
 set LIBS=""
-set COMPILE_FLAGS=-J-Xms2048m -J-Xmx2048m -J-XX:+UseG1GC -Xdiags:verbose -Xlint:all -Xmaxerrs 5 -encoding UTF8 --release 11 -g
+set COMPILE_FLAGS=-J-Xms2048m -J-Xmx2048m -J-XX:+UseG1GC -Xdiags:verbose -Xlint:all -Xmaxerrs 5 -encoding UTF8 --release 17 -g
 
 if exist %OUT_DIR% (
     rmdir /s /q %OUT_DIR%
