6935832805e3210a805ee87794cd2ae7dc1412e6
Niklas Schultz <thexsalkin@gmail.com>
2021-09-19 05:36:31 +0200

    extended FileModWatcher to consider UnsupportedException

--- src/FileModWatcher.java
+++ src/FileModWatcher.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.nio.file.ClosedWatchServiceException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.nio.file.StandardWatchEventKinds;
@@ -11,6 +12,8 @@ public final class FileModWatcher {
 
     private final Path interestingFile;
     private final Lambdas.Nullary<?> callback;
+    private WatchService watcher;
+    private volatile boolean stupidPolling = false;
 
     public FileModWatcher(final Path interestingFile, final Lambdas.Nullary<?> callback) {
         assert interestingFile != null;
@@ -18,18 +21,53 @@ public final class FileModWatcher {
 
         this.interestingFile = interestingFile;
         this.callback        = callback;
+
+        try {
+            watcher = FileSystems.getDefault().newWatchService();
+            Main.logger.log(Level.INFO, "Platform does support event based file watching.");
+        } catch (final IOException ex) {
+            Main.logger.log(Level.SEVERE, ex.getMessage(), ex);
+        } catch (final UnsupportedOperationException ex) {
+            Main.logger.log(Level.WARNING, "Event based file watching is not supported on this platform.");
+        }
     }
 
     public synchronized void start() {
+        if (watcher != null) {
+            smartEventBasedPolling();
+        } else { // @NOTE platform does not support event based file watching
+            stupidPolling();
+        }
+    }
+
+    public synchronized void stop() {
+        if (watcher != null) {
+            try {
+                watcher.close();
+            } catch (final IOException ex) {
+                Main.logger.log(Level.INFO, ex.getMessage(), ex);
+            }
+        } else {
+            stupidPolling = false;
+        }
+    }
+
+    private synchronized void smartEventBasedPolling() {
         try {
-            final WatchService watcher = FileSystems.getDefault().newWatchService();
             Path dir = interestingFile.getParent();
             if (dir == null) { // @NOTE no parent
                 dir = Path.of(System.getProperty("user.dir"));
             }
             dir.register(watcher, StandardWatchEventKinds.ENTRY_MODIFY);
             for (;;) {
-                final WatchKey key = watcher.take();
+
+                WatchKey key = null;
+                try {
+                    key = watcher.take();
+                } catch (final ClosedWatchServiceException ex) {
+                    // @NOTE user has called stop
+                    return;
+                }
 
                 // @NOTE Prevent receiving two separate ENTRY_MODIFY events: file modified
                 // and timestamp updated. Instead, receive one ENTRY_MODIFY event
@@ -63,6 +101,29 @@ public final class FileModWatcher {
             Main.logger.log(Level.SEVERE, ex.getMessage(), ex);
         } catch (final InterruptedException ex) {
             assert false : "Not supposed to interrupt this.";
+        }
+    }
+
+    private synchronized void stupidPolling() {
+        stupidPolling = true;
+
+        long last = interestingFile.toFile().lastModified();
+        while (stupidPolling) {
+            long now = interestingFile.toFile().lastModified();
+            if (now > last) {
+                try {
+                    callback.call();
+                } finally {
+                    last = now;
+                }
+            } else {
+                Thread.yield();
+                try {
+                    Thread.sleep(500);
+                } catch (final InterruptedException ex) {
+                    assert false : "Not supposed to interrupt this.";
+                }
+            }
         }
     }
 }
